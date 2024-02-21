e5180a27353db2b64828f4e099af1579a3dd67d5
Rakeally <40327144+Rakeally@users.noreply.github.com>
2024-02-21 17:50:49 +0100

    update test

--- test/tb_test.js
+++ test/tb_test.js
@@ -1,5 +1,6 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const hre = require("hardhat");
+const { ethers } = require("ethers");
 
 describe("Tokenized bonds Test", () => {
   let tbContract;
@@ -13,14 +14,14 @@ describe("Tokenized bonds Test", () => {
     signers = await hre.ethers.getSigners();
 
     // deploy implementation contract
-    const TBImpl = await ethers.getContractFactory("TBImpl");
+    const TBImpl = await hre.ethers.getContractFactory("TBImpl");
     const tbImpl = await TBImpl.deploy();
 
     await tbImpl.waitForDeployment();
     tbImplementationContract = tbImpl.target;
 
     // deploy implementation proxy contract
-    const ProxyTBImpl = await ethers.getContractFactory("TBProxy");
+    const ProxyTBImpl = await hre.ethers.getContractFactory("TBProxy");
     const proxyTBImpl = await ProxyTBImpl.deploy(tbImplementationContract);
     await proxyTBImpl.waitForDeployment();
 
@@ -49,92 +50,75 @@ describe("Tokenized bonds Test", () => {
 
   describe("Test bond creation", () => {
     it("should fail if caller is not contract owner", async () => {
-      try {
-        const bondParam = {
-          initialSupply: 100000,
-          maturityDate: 1740058156,
-          name: "jane bond",
-          minter: signers[3].getAddress(),
-        };
+      const bondParam = {
+        initialSupply: 100000,
+        maturityDate: 1740058156,
+        name: "jane bond",
+        minter: signers[3].getAddress(),
+      };
 
-        await tbContract
+      await expect(
+        tbContract
           .connect(signers[2])
           .createBond(
             bondParam.initialSupply,
             bondParam.maturityDate,
             bondParam.name,
             bondParam.minter
-          );
-        expect.fail("Expected a revert");
-      } catch (error) {
-        expect(error.message).to.contain("OwnableUnauthorizedAccount");
-      }
+          )
+      ).to.be.rejectedWith("OwnableUnauthorizedAccount");
     });
 
     it("should fail if minter address is invalid", async () => {
-      try {
-        const bondParam = {
-          initialSupply: 100000,
-          maturityDate: 1740058156,
-          name: "CMR BOND",
-          minter: signers[3].getAddress(),
-        };
-        await tbContract.createBond(
+      const bondParam = {
+        initialSupply: 100000,
+        maturityDate: 1740058156,
+        name: "CMR BOND",
+        minter: signers[3].getAddress(),
+      };
+      await expect(
+        tbContract.createBond(
           bondParam.initialSupply,
           bondParam.maturityDate,
           bondParam.name,
           invalidAddress
-        );
-        expect.fail("Expected a revert");
-      } catch (error) {
-        expect(error.message).to.contain(
-          "Address is invalid and Supply below 0"
-        );
-      }
+        )
+      ).to.be.rejectedWith("Address is invalid and Supply below 0");
     });
 
     it("should fail if maturity date is below present date", async () => {
-      try {
-        const bondParam = {
-          initialSupply: 100000,
-          maturityDate: 123,
-          name: "CMR BOND",
-          minter: signers[3].getAddress(),
-        };
-        await tbContract.createBond(
+      const bondParam = {
+        initialSupply: 100000,
+        maturityDate: 123,
+        name: "CMR BOND",
+        minter: signers[3].getAddress(),
+      };
+      await expect(
+        tbContract.createBond(
           bondParam.initialSupply,
           bondParam.maturityDate,
           bondParam.name,
           bondParam.minter
-        );
-        expect.fail("Expected a revert");
-      } catch (error) {
-        expect(error.message).to.contain(
-          "Maturity date must be above current time"
-        );
-      }
+        )
+      ).to.be.revertedWith("Maturity date must be above current time");
     });
 
     it("should fail if initial supply is 0", async () => {
-      try {
-        const bondParam = {
-          initialSupply: 100000,
-          maturityDate: 1740058156,
-          name: "CMR BOND",
-          minter: signers[3].getAddress(),
-        };
-        await tbContract.createBond(
+      const bondParam = {
+        initialSupply: 100000,
+        maturityDate: 1740058156,
+        name: "CMR BOND",
+        minter: signers[3].getAddress(),
+      };
+
+      await expect(
+        tbContract.createBond(
           0,
           bondParam.maturityDate,
           bondParam.name,
           bondParam.minter
-        );
-        expect.fail("Expected a revert");
-      } catch (error) {
-        expect(error.message).to.contain(
-          "Address is invalid and Supply below 0"
-        );
-      }
+        )
+      ).to.be.revertedWith("Address is invalid and Supply below 0");
     });
 
     it("should successfully create Bond", async () => {
@@ -144,36 +128,44 @@ describe("Tokenized bonds Test", () => {
         name: "CMR BOND",
         minter: signers[3].getAddress(),
       };
-      const newBond = await tbContract.createBond(
-        bondParam.initialSupply,
-        bondParam.maturityDate,
-        bondParam.name,
-        bondParam.minter
-      );
 
-      expect(newBond.hash).to.not.undefined;
-      expect(newBond.hash).to.be.a("string");
+      await expect(
+        tbContract
+          .connect(signers[0])
+          .createBond(
+            bondParam.initialSupply,
+            bondParam.maturityDate,
+            bondParam.name,
+            bondParam.minter
+          )
+      )
+        .to.emit(tbContract, "BondCreated")
+        .withArgs(
+          bondParam.minter,
+          bondParam.name,
+          bondParam.maturityDate,
+          BigInt(
+            "94728191037934580968850183277756393853664353133092476056182456800137316456183"
+          )
+        );
     });
 
     it("should fail if trying to recreate an existing bond", async () => {
-      try {
-        const bondParam = {
-          initialSupply: 100000,
-          maturityDate: 1740058156,
-          name: "CMR BOND",
-          minter: signers[3].getAddress(),
-        };
+      const bondParam = {
+        initialSupply: 100000,
+        maturityDate: 1740058156,
+        name: "CMR BOND",
+        minter: signers[3].getAddress(),
+      };
 
-        await tbContract.createBond(
+      await expect(
+        tbContract.createBond(
           bondParam.initialSupply,
           bondParam.maturityDate,
           bondParam.name,
           bondParam.minter
-        );
-        expect.fail("Expected a revert");
-      } catch (error) {
-        expect(error.message).to.contain("Bond already exist");
-      }
+        )
+      ).to.be.rejectedWith("Bond already exist");
     });
   });
 });
