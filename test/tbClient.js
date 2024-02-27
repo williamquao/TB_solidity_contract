d7e97f12ea7b34dd87f5f2dca1aa0397cfac9cf9
Rakeally <40327144+Rakeally@users.noreply.github.com>
2024-02-27 16:55:48 +0100

    Update TB tb client test

--- test/tbClient.js
+++ test/tbClient.js
@@ -12,7 +12,7 @@ describe("Tokenized Bond Test", () => {
     const privateKey = process.env.PRIVATE_KEY;
     const tbClient = new TBClient(privateKey);
     const bondParam = {
-      initialSupply: 100000,
+      initialSupply: 1000000,
       maturityDate: 1735689600,
       name: "CMR Bond",
       minter: "0x40C5537f8b415099278021C5fAaB68989b757e4D",
@@ -28,12 +28,12 @@ describe("Tokenized Bond Test", () => {
     const privateKey = process.env.PRIVATE_KEY;
     const tbClient = new TBClient(privateKey);
 
-    const bondParam = {
+    const depositParam = {
       bondId,
       amount: 5000,
       receiver: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
     };
-    const deposit = await tbClient.deposit(bondParam);
+    const deposit = await tbClient.deposit(depositParam);
     console.log(deposit);
     expect(deposit.hash).to.not.be.undefined;
     expect(deposit.hash).to.be.a("string");
@@ -43,7 +43,7 @@ describe("Tokenized Bond Test", () => {
     const privateKey = process.env.MINTER_PRIVATE_KEY;
     const tbClient = new TBClient(privateKey);
 
-    const bondParam = [
+    const depositParamList = [
       {
         bondId,
         amount: 1000,
@@ -55,7 +55,7 @@ describe("Tokenized Bond Test", () => {
         receiver: "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
       },
     ];
-    const bulkDeposit = await tbClient.bulkDeposit(bondParam);
+    const bulkDeposit = await tbClient.bulkDeposit(depositParamList);
     console.log(bulkDeposit);
     expect(bulkDeposit.hash).to.not.be.undefined;
     expect(bulkDeposit.hash).to.be.a("string");
@@ -65,11 +65,11 @@ describe("Tokenized Bond Test", () => {
     const privateKey = process.env.USER_PRIVATE_KEY;
     const tbClient = new TBClient(privateKey);
 
-    const bondParam = {
+    const withdrawParam = {
       bondId,
       amount: 2000,
     };
-    const withdraw = await tbClient.withdraw(bondParam);
+    const withdraw = await tbClient.withdraw(withdrawParam);
     console.log(withdraw);
     expect(withdraw.hash).to.not.be.undefined;
     expect(withdraw.hash).to.be.a("string");
@@ -79,11 +79,11 @@ describe("Tokenized Bond Test", () => {
     const privateKey = process.env.PRIVATE_KEY;
     const tbClient = new TBClient(privateKey);
 
-    const bondParam = {
+    const minterParam = {
       bondId,
       newMinter: "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
     };
-    const replaceMinter = await tbClient.replaceMinter(bondParam);
+    const replaceMinter = await tbClient.replaceMinter(minterParam);
     console.log(replaceMinter);
     expect(replaceMinter.hash).to.not.be.undefined;
     expect(replaceMinter.hash).to.be.a("string");
@@ -93,72 +93,74 @@ describe("Tokenized Bond Test", () => {
     const privateKey = process.env.PRIVATE_KEY;
     const tbClient = new TBClient(privateKey);
 
-    const bondParam = [
+    const replaceMinterParamList = [
       {
         bondId,
         newMinter: "0x40C5537f8b415099278021C5fAaB68989b757e4D",
       },
     ];
-    const replaceMinter = await tbClient.replaceMintBulk(bondParam);
+    const replaceMinter = await tbClient.replaceMintBulk(
+      replaceMinterParamList
+    );
     console.log(replaceMinter);
     expect(replaceMinter.hash).to.not.be.undefined;
     expect(replaceMinter.hash).to.be.a("string");
   });
 
-  it("should pause a T-bond", async () => {
-    const privateKey = process.env.PRIVATE_KEY;
-    const tbClient = new TBClient(privateKey);
+  //   it("should pause a T-bond", async () => {
+  //     const privateKey = process.env.PRIVATE_KEY;
+  //     const tbClient = new TBClient(privateKey);
 
-    const pauseBond = await tbClient.pauseBond(bondId);
-    console.log(pauseBond);
-    expect(pauseBond.hash).to.not.be.undefined;
-    expect(pauseBond.hash).to.be.a("string");
-  });
+  //     const pauseBond = await tbClient.pauseBond(bondId);
+  //     console.log(pauseBond);
+  //     expect(pauseBond.hash).to.not.be.undefined;
+  //     expect(pauseBond.hash).to.be.a("string");
+  //   });
 
-  it("should resume a T-bond", async () => {
-    const privateKey = process.env.PRIVATE_KEY;
-    const tbClient = new TBClient(privateKey);
+  //   it("should resume a T-bond", async () => {
+  //     const privateKey = process.env.PRIVATE_KEY;
+  //     const tbClient = new TBClient(privateKey);
 
-    const resumeBond = await tbClient.resumeBond(bondId);
-    console.log(resumeBond);
-    expect(resumeBond.hash).to.not.be.undefined;
-    expect(resumeBond.hash).to.be.a("string");
-  });
+  //     const resumeBond = await tbClient.resumeBond(bondId);
+  //     console.log(resumeBond);
+  //     expect(resumeBond.hash).to.not.be.undefined;
+  //     expect(resumeBond.hash).to.be.a("string");
+  //   });
 
-  it("should enable inter-transfer of a T-bond", async () => {
-    const privateKey = process.env.PRIVATE_KEY;
-    const tbClient = new TBClient(privateKey);
+  //   it("should enable inter-transfer of a T-bond", async () => {
+  //     const privateKey = process.env.PRIVATE_KEY;
+  //     const tbClient = new TBClient(privateKey);
 
-    const enableInterTransfer = await tbClient.enableInterTransfer(bondId);
-    console.log(enableInterTransfer);
-    expect(enableInterTransfer.hash).to.not.be.undefined;
-    expect(enableInterTransfer.hash).to.be.a("string");
-  });
+  //     const enableInterTransfer = await tbClient.enableInterTransfer(bondId);
+  //     console.log(enableInterTransfer);
+  //     expect(enableInterTransfer.hash).to.not.be.undefined;
+  //     expect(enableInterTransfer.hash).to.be.a("string");
+  //   });
 
-  it("should disable inter-transfer of a T-bond", async () => {
-    const privateKey = process.env.PRIVATE_KEY;
-    const tbClient = new TBClient(privateKey);
+  //   it("should disable inter-transfer of a T-bond", async () => {
+  //     const privateKey = process.env.PRIVATE_KEY;
+  //     const tbClient = new TBClient(privateKey);
 
-    const disableInterTransfer = await tbClient.disableInterTransfer(bondId);
-    console.log(disableInterTransfer);
-    expect(disableInterTransfer.hash).to.not.be.undefined;
-    expect(disableInterTransfer.hash).to.be.a("string");
-  });
+  //     const disableInterTransfer = await tbClient.disableInterTransfer(bondId);
+  //     console.log(disableInterTransfer);
+  //     expect(disableInterTransfer.hash).to.not.be.undefined;
+  //     expect(disableInterTransfer.hash).to.be.a("string");
+  //   });
 
-  it("should disable inter-transfer of a T-bond", async () => {
-    const privateKey = process.env.USER_PRIVATE_KEY;
-    const tbClient = new TBClient(privateKey);
+  //   it("should disable inter-transfer of a T-bond", async () => {
+  //     const privateKey = process.env.USER_PRIVATE_KEY;
+  //     const tbClient = new TBClient(privateKey);
 
-    const transferParam = {
-      bondId,
-      amount: 1000,
-      newMinter: "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
-    };
-    const transferBondAmongUsers = await tbClient.transferBondAmongUsers(
-      transferParam
-    );
-    console.log(transferBondAmongUsers);
-    expect(transferBondAmongUsers.hash).to.not.be.undefined;
-    expect(transferBondAmongUsers.hash).to.be.a("string");
-  });
+  //     const transferParam = {
+  //       bondId,
+  //       amount: 1000,
+  //       newMinter: "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+  //     };
+  //     const transferBondAmongUsers = await tbClient.transferBondAmongUsers(
+  //       transferParam
+  //     );
+  //     console.log(transferBondAmongUsers);
+  //     expect(transferBondAmongUsers.hash).to.not.be.undefined;
+  //     expect(transferBondAmongUsers.hash).to.be.a("string");
+  //   });
 });
