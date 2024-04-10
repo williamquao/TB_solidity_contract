718e21a9cf7c2f8aa517ad9b4f1de3aba18507d7
rakeally <rakeally@gmail.com>
2024-04-10 13:43:24 +0100

    update index file

--- client/index.ts
+++ client/index.ts
@@ -2,19 +2,22 @@ import { TBClient } from "../client/tb-client";
 import { Transfer } from "../dto/tb.dto";
 
 (async () => {
-  const privateKey = 'privateKey here';
+  const privateKey = "privateKey here";
   const transfers: Transfer[] = [
     {
-      tokenId: 1,
-      amount: 100000,
-      sender: "0xd59ba1d313685E79753Fbe3dAe0FD60a01BE79F3",
-      receiver: "0x435E4c824E5913180B724BA34c28c1ea61cBC8E6",
-    },
-    {
-      tokenId: 2,
-      amount: 5000,
-      sender: "0x435E4c824E5913180B724BA34c28c1ea61cBC8E6",
-      receiver: "0xd59ba1d313685E79753Fbe3dAe0FD60a01BE79F3",
+      from: "0xd59ba1d313685E79753Fbe3dAe0FD60a01BE79F3",
+      transferDest: [
+        {
+          tokenId: 1,
+          amount: 100000,
+          receiver: "0x435E4c824E5913180B724BA34c28c1ea61cBC8E6",
+        },
+        {
+          tokenId: 2,
+          amount: 5000,
+          receiver: "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4",
+        },
+      ],
     },
   ];
 
@@ -22,5 +25,3 @@ import { Transfer } from "../dto/tb.dto";
 
   await tbClient.transfer(transfers);
 })();
-
-
