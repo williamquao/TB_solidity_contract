876fbf8a409ea550b5ffb1fbc5e10763e23679bc
_mr_quao <38799705+williamquao@users.noreply.github.com>
2024-05-13 16:18:59 +0000

    hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,22 +9,4 @@ module.exports = {
     enabled: true,
     runs: 200, 
   },
-  defaultNetwork: "baseSepolia",
-  networks: {
-    sepolia: {
-      url: "https://rpc2.sepolia.org",
-      chainId: 11155111,
-      accounts: [process.env.TESTNET_PRIVATE_KEY],
-    },
-    baseSepolia: {
-      url: "https://base-sepolia.public.blastapi.io",
-      chainId: 84532,
-      accounts: [process.env.BASE_TESTNET_PRIVATE_KEY],
-    },
-    baseMainnet: {
-      url: "https://base-mainnet.public.blastapi.io",
-      chainId: 8453,
-      accounts: [process.env.MAINNET_PRIVATE_KEY],
-    },
-  },
 };
